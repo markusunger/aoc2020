5979367c833241d16ec63c345610b8f29f0ce078
Markus Unger <schaulustig23@gmail.com>
2021-09-18 14:02:50 +0200

    add raw input utility method, add explicit types

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,9 +1,17 @@
 import { readFile } from 'fs';
 import { promisify } from 'util';
 
+export type Input = string[];
+export type RawInput = string;
+
 const readFileAsync = promisify(readFile);
 
 export const getInput = async (fileName: string): Promise<string[]> => {
     const input = await readFileAsync(fileName, { encoding: 'utf-8' });
     return input.split('\n');
+};
+
+export const getRawInput = async (fileName: string): Promise<string> => {
+    const input = await readFileAsync(fileName, { encoding: 'utf-8' });
+    return input;
 };
